bbefadcf59fb006ca86d6a4fb989039736a8bf61
Alison8547 <alisonailson100@hotmail.com>
2023-09-15 21:57:36 -0300

    feat: Add repository Loan and Customer

---
+++ src/main/java/com/br/loanservice/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.br.loanservice.repository;
+
+import com.br.loanservice.domain.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Integer> {
+}
