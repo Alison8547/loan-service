38279698809b754c26b953db7a65aec1f264da84
Alison8547 <alisonailson100@hotmail.com>
2023-09-19 12:05:39 -0300

    docs: swagger

--- src/main/java/com/br/loanservice/controller/CustomerController.java
+++ src/main/java/com/br/loanservice/controller/CustomerController.java
@@ -4,6 +4,9 @@ import com.br.loanservice.dto.response.CustomerLoanResponse;
 import com.br.loanservice.dto.response.CustomerResponse;
 import com.br.loanservice.dto.resquest.CustomerRequest;
 import com.br.loanservice.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +24,27 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    @Operation(summary = "Create customer", description = "Save a customer in the database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Successfully created"),
+                    @ApiResponse(responseCode = "403", description = "You do not have permission to access this resource."),
+                    @ApiResponse(responseCode = "400", description = "An exception was generated")
+            }
+    )
     @PostMapping("/create-customer")
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody @Valid CustomerRequest customerRequest) {
         return new ResponseEntity<>(customerService.createCustomer(customerRequest), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "List customer loan", description = "Returns a list")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Returns successfully!"),
+                    @ApiResponse(responseCode = "403", description = "You do not have permission to access this resource."),
+                    @ApiResponse(responseCode = "400", description = "An exception was generated")
+            }
+    )
     @GetMapping("/list-customer-loan")
     public ResponseEntity<List<CustomerLoanResponse>> listCustomerLoan(@RequestParam(required = false) Integer idCustomer) {
         return new ResponseEntity<>(customerService.listCustomerLoan(idCustomer), HttpStatus.OK);
--- src/main/java/com/br/loanservice/controller/LoanController.java
+++ src/main/java/com/br/loanservice/controller/LoanController.java
@@ -4,6 +4,9 @@ import com.br.loanservice.dto.response.LoanResponse;
 import com.br.loanservice.dto.resquest.LoanRequest;
 import com.br.loanservice.service.LoanService;
 import com.br.loanservice.service.PdfService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +26,28 @@ public class LoanController {
     private final LoanService loanService;
     private final PdfService pdfService;
 
+
+    @Operation(summary = "Open loan for the customer", description = "Save a loan in the database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Successfully created"),
+                    @ApiResponse(responseCode = "403", description = "You do not have permission to access this resource."),
+                    @ApiResponse(responseCode = "400", description = "An exception was generated")
+            }
+    )
     @PostMapping("/open-loan/{idCustomer}")
     public ResponseEntity<LoanResponse> openLoan(@PathVariable(name = "idCustomer") Integer idCustomer, @RequestBody @Valid LoanRequest loanRequest) {
         return new ResponseEntity<>(loanService.openLoan(idCustomer, loanRequest), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Generated pdf the loan", description = "Returns a pdf")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Returns successfully!"),
+                    @ApiResponse(responseCode = "403", description = "You do not have permission to access this resource."),
+                    @ApiResponse(responseCode = "400", description = "An exception was generated")
+            }
+    )
     @GetMapping("/generated-pdf-loan/{idLoan}")
     public ResponseEntity<Void> generatedPdfLoan(@PathVariable(name = "idLoan") Integer idLoan, HttpServletResponse httpServletResponse) throws IOException {
         pdfService.generatedPdfLoan(idLoan, httpServletResponse);
